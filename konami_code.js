b88591f5a114b22f738044e26468bdd197461768
Eric Roginek <eroginek@protonmail.com>
2017-10-03 00:34:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,16 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+  const body = document.body
+  var i = 0
+    function onKey(e) {
+      console.log(e.which)
+      if (e.which === code[i]){
+        i++
+        if (i === code.length - 1){
+          console.log(alert("You did it!"))
+          i = 0
+        }
+      }
+    }
+  body.addEventListener('keydown', onKey)
 }
